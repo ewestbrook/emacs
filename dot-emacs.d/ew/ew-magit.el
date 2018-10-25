a462b5172ec84e67a97b6036d201fb7b1b8fcfbd
E. Westbrook <e@westbrook.io>
2018-10-25 10:20:52 -0600

    ew-magit.el: comments; also placeholder for currently not working magit-section-initial-visibility-list

--- dot-emacs.d/ew/ew-magit.el
+++ dot-emacs.d/ew/ew-magit.el
@@ -1,6 +1,10 @@
-;; emacs -*- mode: emacs-lisp; -*-
-;; ew-magit.el
+;;; ew-magit -- Summery -*- mode: emacs-lisp; -*-
+;;; Commentary:
+;;; Code:
 ;; -------------------------------------------------------
 (setq magit-status-buffer-switch-function 'switch-to-buffer)
 (load "magit-filenotify")
+;; (add-to-list 'magit-section-initial-visibility-alist '(@{upstream} . show))
 ;; -------------------------------------------------------
+(provide 'ew-magit)
+;;; ew-magit.el ends here
