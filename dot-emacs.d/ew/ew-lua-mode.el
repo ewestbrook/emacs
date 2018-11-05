91e0af8e50d61159982d364ad6c100f3a0ab99d4
E. Westbrook <e@westbrook.io>
2018-11-04 17:29:55 -0700

    ew-lua-mode.el: setq -> setq-default

--- dot-emacs.d/ew/ew-lua-mode.el
+++ dot-emacs.d/ew/ew-lua-mode.el
@@ -1,16 +1,17 @@
 ;;; ew-lua-mode.el --- Summary
 ;;; Commentary:
 ;;; Code:
-
+;; -------------------------------------------------------
 (require 'lua-mode)
-
-(setq lua-indent-level 2)
-;; (setq lua-indent-level 4)
-
+;; -------------------------------------------------------
+(setq-default lua-indent-level 2)
+;; (setq-default lua-indent-level 2)
+;; (setq-default lua-indent-level 4)
+;; -------------------------------------------------------
 (autoload 'lua-mode "lua-mode" "Lua editing mode." t)
-
+;; -------------------------------------------------------
 (add-to-list 'auto-mode-alist '("\\.lua$" . lua-mode))
 (add-to-list 'interpreter-mode-alist '("lua" . lua-mode))
-
+;; -------------------------------------------------------
 (provide 'ew-lua-mode)
 ;;; ew-lua-mode ends here
