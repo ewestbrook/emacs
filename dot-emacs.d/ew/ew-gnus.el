ce418673a12dace10470c1ee722ef40950764261
E. Westbrook <e@westbrook.io>
2018-11-04 17:29:19 -0700

    ew-gnus.el: package header/footer; setq -> setq-default

--- dot-emacs.d/ew/ew-gnus.el
+++ dot-emacs.d/ew/ew-gnus.el
@@ -1,14 +1,20 @@
+;;; ew-gnus -- Summary
+;;; Commentary:
+;;; Code:
 ;; -------------------------------------------------------
 ;; gnus
 
-(setq gnus-select-method
-      '(nnimap "mail.westbrook.io"
-               (nnimap-server-port 9993)
-               (nnimap-stream ssl)))
+(setq-default
+ gnus-select-method
+ '(nnimap "mail.westbrook.io"
+          (nnimap-server-port 9993)
+          (nnimap-stream ssl)))
 
-(setq gnus-secondary-select-methods
-      '((nnimap "mail.vmedex.net"
-               (nnimap-server-port 993)
-               (nnimap-stream ssl))))
-
+(setq-default
+ gnus-secondary-select-methods
+ '((nnimap "mail.vmedex.net"
+           (nnimap-server-port 993)
+           (nnimap-stream ssl))))
 ;; -------------------------------------------------------
+(provide 'ew-gnus)
+;;; ew-gnus ends here
