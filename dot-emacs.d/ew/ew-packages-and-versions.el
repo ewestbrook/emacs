67d50865398d0b0414a5eb1f1a8c47faf95765aa
E. Westbrook <e@westbrook.io>
2018-11-04 17:31:09 -0700

    ew-packages-and-versions.el: gone

--- dot-emacs.d/ew/ew-packages-and-versions.el
+++
@@ -1,8 +0,0 @@
-(defun list-packages-and-versions ()
-  "Returns a list of all installed packages and their versions"
-  (mapcar
-   (lambda (pkg)
-     `(,pkg ,(package-desc-version
-                (cadr (assq pkg package-alist)))))
-   package-activated-list))
-;; (list-packages-and-versions)
