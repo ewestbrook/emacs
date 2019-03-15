94a8c57aa4ca096b0c658df664fd7470dcf7535f
E. Westbrook <e@westbrook.io>
2019-03-15 02:42:09 -0600

    ew-org-mode: org -> eo

--- dot-emacs.d/ew/ew-org-mode.el
+++ dot-emacs.d/ew/ew-org-mode.el
@@ -117,34 +117,105 @@
 ;; org-publish
 (setq-default
      org-publish-project-alist
-     '(("gitorg"
-        :base-directory "/git/org"
-        :base-extension "org"
-        :publishing-directory "/git/org/h"
-        :recursive t
-        :publishing-function org-twbs-publish-to-html
-        :auto-preamble t
-        :auto-sitemap t
-        :sitemap-filename "sitenodes.org"
-        :sitemap-ignore-case t
-        :sitemap-title "All Nodes")
-       ("gitorg-static"
-        :base-directory "/git/org"
-        :base-extension "pdf\\|mp3\\|mp4\\|mkv\\|txt\\|eml"
-        :publishing-directory "/git/org/h"
-        :recursive t
-        :publishing-function org-publish-attachment)
+     '(
+
        ("w"
         :base-directory "/git/w/src"
         :base-extension "org"
         :publishing-directory "/git/w/h"
         :recursive t
         :publishing-function org-twbs-publish-to-html
-        :auto-preamble t
-        :auto-sitemap t
-        :sitemap-filename "sitemap.org"
-        :sitemap-ignore-case t
-        :sitemap-title "Site Map")))
+        :auto-preamble nil)
+
+       ("ew"
+        :base-directory "/git/eo/ew"
+        :base-extension "org"
+        :publishing-directory "/git/eo/h/ew"
+        :recursive t
+        :publishing-function org-twbs-publish-to-html
+        :auto-preamble nil)
+
+       ("eewa"
+        :base-directory "/git/eo/eewa"
+        :base-extension "org"
+        :publishing-directory "/git/eo/h/eewa"
+        :recursive t
+        :publishing-function org-twbs-publish-to-html
+        :auto-preamble nil)
+
+       ("cv"
+        :base-directory "/git/eo/cv"
+        :base-extension "org"
+        :publishing-directory "/git/eo/h/cv"
+        :recursive t
+        :publishing-function org-twbs-publish-to-html
+        :auto-preamble nil)
+
+       ("expert"
+        :base-directory "/git/eo/expert"
+        :base-extension "org"
+        :publishing-directory "/git/eo/h/expert"
+        :recursive t
+        :publishing-function org-twbs-publish-to-html
+        :auto-preamble nil)
+
+       ("guru"
+        :base-directory "/git/eo/guru"
+        :base-extension "org"
+        :publishing-directory "/git/eo/h/guru"
+        :recursive t
+        :publishing-function org-twbs-publish-to-html
+        :auto-preamble nil)
+
+       ("eng"
+        :base-directory "/git/eo/eng"
+        :base-extension "org"
+        :publishing-directory "/git/eo/h/eng"
+        :recursive t
+        :publishing-function org-twbs-publish-to-html
+        :auto-preamble nil)
+
+       ("minme"
+        :base-directory "/git/eo/minme"
+        :base-extension "org"
+        :publishing-directory "/git/eo/h/minme"
+        :recursive t
+        :publishing-function org-twbs-publish-to-html
+        :auto-preamble nil)
+
+       ("schnauzers"
+        :base-directory "/git/eo/schnauzers"
+        :base-extension "org"
+        :publishing-directory "/git/eo/h/schnauzers"
+        :recursive t
+        :publishing-function org-twbs-publish-to-html
+        :auto-preamble nil)
+
+       ("smd"
+        :base-directory "/git/eo/smd"
+        :base-extension "org"
+        :publishing-directory "/git/eo/h/smd"
+        :recursive t
+        :publishing-function org-twbs-publish-to-html
+        :auto-preamble nil)
+
+       ("today"
+        :base-directory "/git/eo/today"
+        :base-extension "org"
+        :publishing-directory "/git/eo/h/today"
+        :recursive t
+        :publishing-function org-twbs-publish-to-html
+        :auto-preamble nil)
+
+       ("wwwwc"
+        :base-directory "/git/eo/wwwwc"
+        :base-extension "org"
+        :publishing-directory "/git/eo/h/wwwwc"
+        :recursive t
+        :publishing-function org-twbs-publish-to-html
+        :auto-preamble nil)
+
+       ))
 ;; -------------------------------------------------------
 ;; ew functions
 ;; -------------------------------------------------------
