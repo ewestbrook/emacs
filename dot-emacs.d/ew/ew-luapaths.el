5e4c9db540f40b228601a5c5977bb363432c2b7a
Eric E. Westbrook <eew@lanl.gov>
2019-06-17 12:56:00 -0600

    ew/ew-{pathenv,luapaths}.el

---
+++ dot-emacs.d/ew/ew-luapaths.el
@@ -0,0 +1,47 @@
+;;; ew-luapaths --- Summary
+;;; Commentary:
+;;; Code:
+;; -------------------------------------------------------
+(require 'ew-pathenv)
+;; -------------------------------------------------------
+(setq-default
+ ew-luapaths-lua-paths
+ (list
+  (concat (getenv "HOME") "/share/lua/5.1/?.lua")
+  (concat (getenv "HOME") "/git/w/src/?.lua")))
+;; -------------------------------------------------------
+(setq-default
+ ew-luapaths-lua-cpaths
+ (list
+  (concat (getenv "HOME") "/lib/lua/5.1/?.so")
+  (concat (getenv "HOME") "/git/w/src/?.so")))
+;; -------------------------------------------------------
+(defun ew-luapaths-prepend-paths()
+  "Prepend paths in 'ew-pathenv-lua-paths' to env's LUA_PATH uniquely, and similarly with ew-pathenv-clua-paths and LUA_CPATH, retaining trailing semicolons or adding them if the path was originally empty."
+  (let* ((lp (getenv "LUA_PATH"))
+         (clp (getenv "LUA_CPATH"))
+         (lpdef (or (s-blank? lp)
+                    (string-match ";;$" lp)))
+         (clpdef (or (s-blank? clp)
+                     (string-match ";;$" clp))))
+    (mapcar 'ew-luapaths-prepend-path
+            ew-luapaths-lua-paths)
+    (mapcar 'ew-luapaths-prepend-cpath
+            ew-luapaths-lua-cpaths)
+    (cond (lpdef
+           (setenv "LUA_PATH"
+                   (concat (getenv "LUA_PATH") ";;"))))
+    (cond (clpdef
+           (setenv "LUA_CPATH"
+                   (concat (getenv "LUA_CPATH") ";;"))))))
+;; -------------------------------------------------------
+(defun ew-luapaths-prepend-path(v)
+  "Prepend path V to env's LUA_PATH uniquely."
+  (ew-pathenv-prepend-env-path "LUA_PATH" v ";"))
+;; -------------------------------------------------------
+(defun ew-luapaths-prepend-cpath(v)
+  "Prepend path V to env's LUA_CPATH uniquely."
+  (ew-pathenv-prepend-env-path "LUA_CPATH" v ";"))
+;; -------------------------------------------------------
+(provide 'ew-luapaths)
+;;; ew-luapaths ends here
---
+++ dot-emacs.d/ew/ew-pathenv.el
@@ -0,0 +1,54 @@
+;;; ew-pathenv --- Summary
+;;; Commentary:
+;;; Code:
+;; -------------------------------------------------------
+(require 's)
+;; -------------------------------------------------------
+(setq-default ew-pathenv-home (getenv "HOME"))
+;; -------------------------------------------------------
+(setq-default
+ ew-pathenv-paths
+ (append
+  '("/usr/local/opt/gnu-sed/libexec/gnubin"
+    "/usr/local/opt/texinfo/bin"
+    "/Applications/Firefox.app/Contents/MacOS"
+    "/usr/local/bin"
+    "/usr/local/sbin")
+  (list (concat ew-pathenv-home "/bin"))))
+;; -------------------------------------------------------
+(defun ew-pathenv-prepend-paths()
+  "Prepend paths in 'ew-pathenv-paths' to env's PATH and 'exec-path' uniquely, for each element that exists as a directory."
+  (mapcar 'ew-pathenv-prepend-path-dirs ew-pathenv-paths))
+;; -------------------------------------------------------
+(defun ew-pathenv-prepend-path-dirs(v)
+  "Prepend path V to env's PATH and 'exec-path' uniquely, if V exists as a directory."
+  (cond
+   ((file-directory-p v)
+    (ew-pathenv-prepend-path v))))
+;; -------------------------------------------------------
+(defun ew-pathenv-prepend-path(v)
+  "Prepend path V to env's PATH and 'exec-path' uniquely."
+  (ew-pathenv-prepend-env-path "PATH" v)
+  (ew-pathenv-prepend-exec-path v))
+;; -------------------------------------------------------
+(defun ew-pathenv-prepend-env-path(e v &optional sep)
+  "Prepend V to environment variable E (separated by SEP) uniquely."
+  (let* ((s (if sep sep ":"))
+        (sv (split-string v s t)))
+    (car (last (mapcar (lambda(arg)
+              (ew-pathenv-prepend-env-path-single e arg sep))
+            (split-string v s t))))))
+;; -------------------------------------------------------
+(defun ew-pathenv-prepend-env-path-single(e v &optional sep)
+  "Prepend V to environment variable E (separated by SEP) uniquely; V must already be separated by SEP."
+  (let ((s (if sep sep ":"))
+        (ev (getenv e)))
+    (setenv e (if (s-blank? ev) v (s-join s (cons v (delete v (split-string ev s t))))))))
+;; -------------------------------------------------------
+(defun ew-pathenv-prepend-exec-path(v)
+  "Prepend path V to 'exec-path' uniquely."
+  (delete v exec-path)
+  (add-to-list 'exec-path v))
+;; -------------------------------------------------------
+(provide 'ew-pathenv)
+;;; ew-pathenv ends here
