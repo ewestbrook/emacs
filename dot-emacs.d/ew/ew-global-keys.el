23906fa86bf66fc389c328132220b7cbac0a3773
Eric E. Westbrook <eew@lanl.gov>
2019-06-24 10:59:32 -0600

    ew-global-keys: reorg, comment

--- dot-emacs.d/ew/ew-global-keys.el
+++ dot-emacs.d/ew/ew-global-keys.el
@@ -4,34 +4,48 @@
 ;; -------------------------------------------------------
 (message "loading ew-global-keys")
 ;; -------------------------------------------------------
-;; keys
+;; misc
+(global-set-key (kbd "M-S-SPC") 'set-mark)
+(global-set-key (kbd "C-x M-e") 'eval-buffer)
+;; -------------------------------------------------------
+;; org
 (global-set-key "\C-cl" 'org-store-link)
 (global-set-key "\C-ca" 'org-agenda)
 (global-set-key "\C-cc" 'org-capture)
 (global-set-key "\C-cb" 'org-switchb)
+;; -------------------------------------------------------
+;; magit
 (global-set-key "\C-xg" 'magit-status)
+;; -------------------------------------------------------
+;; ew
 (global-set-key "\C-x\C-t" 'toggle-truncate-lines)
 (global-set-key "\C-x\C-u" 'toggle-current-window-dedication)
+(global-set-key [pause] 'toggle-current-window-dedication)
+(global-set-key (kbd "M-p") 'ew-toggle-clipboard-mode)
+;; -------------------------------------------------------
+;; embiggen/enshrinken
+(global-set-key (kbd "C-+") 'text-scale-increase)
+(global-set-key (kbd "C--") 'text-scale-decrease)
 (global-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
 (global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
 (global-set-key (kbd "S-C-<down>") 'shrink-window)
-(global-set-key (kbd "S-C-<up>"  ) 'enlarge-window)
-(global-set-key (kbd "C-M-g"     ) 'grep)
-;; (global-set-key [f7] 'my-compile)
-(global-set-key (kbd"<f5>"    ) 'gud-cont)
+(global-set-key (kbd "S-C-<up>") 'enlarge-window)
+;; -------------------------------------------------------
+;; grep
+(global-set-key (kbd "C-M-g") 'grep)
+;; -------------------------------------------------------
+;; gud et al
+(global-set-key (kbd"<f5>") 'gud-cont)
 ;; (global-set-key (kbd"<f6>") 'gud-break)
-(global-set-key (kbd"<f7>"    ) 'gud-finish)
-(global-set-key (kbd"<f8>"    ) 'gud-next)
-(global-set-key (kbd"<f9>"    ) 'gud-watch)
-(global-set-key (kbd"<f10>"   ) 'gud-step)
-(global-set-key (kbd "M-s b"  ) 'ediff-buffers)
-(global-set-key (kbd "M-s v"  ) 'ediff-revision)
-(global-set-key (kbd "M-p"    ) 'ew-toggle-clipboard-mode)
-(global-set-key (kbd "C-+"    ) 'text-scale-increase)
-(global-set-key (kbd "C--"    ) 'text-scale-decrease)
-(global-set-key (kbd "M-S-SPC") 'set-mark)
-(global-set-key (kbd "C-x M-e") 'eval-buffer)
-(global-set-key [pause] 'toggle-current-window-dedication)
+(global-set-key (kbd"<f7>") 'gud-finish)
+;; (global-set-key [f7] 'my-compile)
+(global-set-key (kbd"<f8>") 'gud-next)
+(global-set-key (kbd"<f9>") 'gud-watch)
+(global-set-key (kbd"<f10>") 'gud-step)
+;; -------------------------------------------------------
+;; ediff
+(global-set-key (kbd "M-s b") 'ediff-buffers)
+(global-set-key (kbd "M-s v") 'ediff-revision)
 ;; -------------------------------------------------------
 ;; smex
 (global-set-key (kbd "M-x") 'smex)
