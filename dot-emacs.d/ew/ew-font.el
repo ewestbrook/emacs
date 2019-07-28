cdd834f792311f241323be05e651ed8720aaff77
Eric Westbrook <e@westbrook.io>
2019-07-28 00:47:42 -0600

    ew-font: a1 menlo 13

--- dot-emacs.d/ew/ew-font.el
+++ dot-emacs.d/ew/ew-font.el
@@ -91,7 +91,7 @@
  ew-host-font-map
  (list
   (cons "^p13[ab]"      "terminus 14")
-  (cons "^a1\\."        "Menlo 16")
+  (cons "^a1\\."        "Menlo 13")
   (cons "^viking\\."    "terminus 9")
   (cons "^e1\\."        "xos4 terminus 9")
   (cons "^edoras\\."    "xos4 terminus 9")
