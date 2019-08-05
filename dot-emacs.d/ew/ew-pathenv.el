3f4e4f9e2d5d7e9a902863d0b91fb4967b2dba46
Eric Westbrook <e@westbrook.io>
2019-08-04 19:42:36 -0600

    ew/ew-pathenv.el: add /usr/local/texlive/2019/bin/x86_64-darwin

--- dot-emacs.d/ew/ew-pathenv.el
+++ dot-emacs.d/ew/ew-pathenv.el
@@ -14,6 +14,7 @@
   '("/usr/local/opt/gnu-sed/libexec/gnubin"
     "/usr/local/opt/texinfo/bin"
     "/Applications/Firefox.app/Contents/MacOS"
+    "/usr/local/texlive/2019/bin/x86_64-darwin"
     "/usr/local/bin"
     "/usr/local/sbin")
   (list (concat ew-pathenv-home "/bin"))
