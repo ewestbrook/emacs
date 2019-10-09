3f2b10352e64619b953c4a39a4d45772fdbc9760
Eric E. Westbrook <eew@lanl.gov>
2019-10-09 07:23:53 -0600

    ew-hs-minor-mode-settings

---
+++ dot-emacs.d/ew/ew-hs-minor-mode-settings.el
@@ -0,0 +1,16 @@
+;;; ew-hs-minor-mode-settings -- Summary
+;;; Commentary:
+;;; Code:
+;; -------------------------------------------------------
+(message "loading ew-hs-minor-mode-settings")
+;; -------------------------------------------------------
+(define-key hs-minor-mode-map (kbd "C-c TAB") 'hs-toggle-hiding)
+;; -------------------------------------------------------
+(defun hs-minor-mode-on()
+  (interactive)
+  (hs-minor-mode 1))
+;; -------------------------------------------------------
+(add-hook 'python-mode-hook 'hs-minor-mode-on)
+;; -------------------------------------------------------
+(provide 'ew-hs-minor-mode-settings)
+;;; ew-hs-minor-mode-settings ends here
