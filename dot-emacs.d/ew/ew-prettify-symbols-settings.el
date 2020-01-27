36c985d019e09878c15e1450e8a5425e22853f06
Eric Westbrook <e@westbrook.io>
2020-01-26 17:11:16 -0700

    unicode, prettify

---
+++ dot-emacs.d/ew/ew-prettify-symbols-settings.el
@@ -0,0 +1,20 @@
+;;; ew-prettify-symbols-settings --- Summary
+;;; Commentary:
+;;; Code:
+;; -------------------------------------------------------
+;; see
+;; https://emacsredux.com/blog/2014/08/25/a-peek-at-emacs-24-dot-4-prettify-symbols-mode/
+;; https://github.com/tonsky/FiraCode/wiki/Emacs-instructions
+;; -------------------------------------------------------
+(defconst lisp--prettify-symbols-alist
+  '(("lambda"  . ?λ)))
+;; -------------------------------------------------------
+(add-hook
+ 'emacs-lisp-mode-hook
+ (lambda ()
+   (push '(">=" . ?≥) prettify-symbols-alist)
+   (push '("<=" . ?≤) prettify-symbols-alist)
+   ))
+;; -------------------------------------------------------
+(provide 'ew-prettify-symbols-settings)
+;;; ew-prettify-symbols-settings ends here
