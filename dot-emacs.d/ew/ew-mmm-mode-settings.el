08bd4ee4c7c12036f0f217b9246d28320eac5667
Eric E. Westbrook <git@westbrook.io>
2020-02-09 01:32:17 -0700

    mmm-mode

---
+++ dot-emacs.d/ew/ew-mmm-mode-settings.el
@@ -0,0 +1,9 @@
+;;; ew-mmm-mode-settings --- Summary
+;;; Commentary:
+;;; Code:
+;; -------------------------------------------------------
+(setq-default mmm-global-mode 'maybe)
+(mmm-add-mode-ext-class 'html-mode "\\.php\\'" 'html-php)
+;; -------------------------------------------------------
+(provide 'ew-mmm-mode-settings)
+;;; ew-mmm-mode-settings ends here
