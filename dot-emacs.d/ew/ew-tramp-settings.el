f2e8560c39158bc88d8ad8ed131d40c2f09e4922
Eric E. Westbrook <eew@lanl.gov>
2020-02-18 11:23:15 -0700

    ew/ew-tramp-settings.el

---
+++ dot-emacs.d/ew/ew-tramp-settings.el
@@ -0,0 +1,12 @@
+;;; ew-tramp-settings --- Summary
+;;; Commentary:
+;;; Code:
+;; -------------------------------------------------------
+(message "loading ew-tramp-settings")
+;; -------------------------------------------------------
+(setq-default tramp-default-method "ssh")
+;; (setq-default tramp-persistency-file-name nil)
+;; (add-to-list 'tramp-remote-path "/usr/lcs/local/bin")
+;; -------------------------------------------------------
+(provide 'ew-tramp-settings)
+;;; ew-tramp-settings ends here
