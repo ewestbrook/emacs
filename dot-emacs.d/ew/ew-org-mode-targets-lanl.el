ebae280270d1be6bb6ebccb5499c99f956d5d790
Eric E. Westbrook <eew+git@lanl.gov>
2020-05-11 20:33:01 -0600

    targets-lanl: duplicate due to macOS realpathing on us without warning

--- dot-emacs.d/ew/ew-org-mode-targets-lanl.el
+++ dot-emacs.d/ew/ew-org-mode-targets-lanl.el
@@ -8,34 +8,82 @@
  org-publish-project-alist
  '(
 
-   ("lanlpres"
-    :base-directory "~/git/eew-lanlpres"
+   ("lanlpres-home"
+    :base-directory "~/git/eew/lanlpres"
     :base-extension "org"
-    :publishing-directory "~/git/eew-lanlpres-h"
+    :publishing-directory "~/git/eew/lanlpres-h"
     :recursive t
     :publishing-function org-twbs-publish-to-html
     :auto-preamble nil)
 
-   ("mgmt"
-    :base-directory "~/git/eew-mgmt"
+   ("lanlpres-volume"
+    :base-directory "/Volumes/git/eew/lanlpres"
     :base-extension "org"
-    :publishing-directory "~/git/eew-mgmt-h"
+    :publishing-directory "/Volumes/git/eew/lanlpres-h"
     :recursive t
     :publishing-function org-twbs-publish-to-html
     :auto-preamble nil)
 
-   ("w"
-    :base-directory "~/git/w/src"
+   ("mgmt-home"
+    :base-directory "~/git/eew/mgmt"
     :base-extension "org"
-    :publishing-directory "~/git/w-h"
+    :publishing-directory "~/git/eew/mgmt-h"
     :recursive t
     :publishing-function org-twbs-publish-to-html
     :auto-preamble nil)
 
-   ("lcsdemo"
+   ("mgmt-volume"
+    :base-directory "/Volumes/git/eew/mgmt"
+    :base-extension "org"
+    :publishing-directory "/Volumes/git/eew/mgmt-h"
+    :recursive t
+    :publishing-function org-twbs-publish-to-html
+    :auto-preamble nil)
+
+   ("w-home"
+    :base-directory "~/git/eew/w/src"
+    :base-extension "org"
+    :publishing-directory "~/git/eew/w-h"
+    :recursive t
+    :publishing-function org-twbs-publish-to-html
+    :auto-preamble nil)
+
+   ("w-volume"
+    :base-directory "/Volumes/git/eew/w/src"
+    :base-extension "org"
+    :publishing-directory "/Volumes/git/eew/w-h"
+    :recursive t
+    :publishing-function org-twbs-publish-to-html
+    :auto-preamble nil)
+
+   ("lcsdemo-home"
     :base-directory "~/git/eew-lcsdemo"
     :base-extension "org"
     :publishing-directory "~/git/eew-lcsdemo-h"
+    :recursive t
+    :publishing-function org-twbs-publish-to-html
+    :auto-preamble nil)
+
+   ("lcsdemo-volume"
+    :base-directory "/Volumes/git/eew-lcsdemo"
+    :base-extension "org"
+    :publishing-directory "/Volumes/git/eew-lcsdemo-h"
+    :recursive t
+    :publishing-function org-twbs-publish-to-html
+    :auto-preamble nil)
+
+   ("lcslinux-home"
+    :base-directory "~/git/aot-ic/lcslinux"
+    :base-extension "org"
+    :publishing-directory "~/git/aot-ic/lcslinux-h"
+    :recursive t
+    :publishing-function org-twbs-publish-to-html
+    :auto-preamble nil)
+
+   ("lcslinux-volume"
+    :base-directory "/Volumes/git/aot-ic/lcslinux"
+    :base-extension "org"
+    :publishing-directory "/Volumes/git/aot-ic/lcslinux-h"
     :recursive t
     :publishing-function org-twbs-publish-to-html
     :auto-preamble nil)
