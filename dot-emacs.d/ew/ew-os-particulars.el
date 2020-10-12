28c0e545d6b04a200248e34d7b081da7820bfeaf
Eric E. Westbrook <eew+git@lanl.gov>
2020-10-12 22:09:28 +0000

    ew-os-particulars.el: add (commented) super key bindings

--- dot-emacs.d/ew/ew-os-particulars.el
+++ dot-emacs.d/ew/ew-os-particulars.el
@@ -29,6 +29,11 @@
     (setq-default browse-url-browser-function 'browse-url-firefox)
     (setq-default browse-url-new-window-flag  t)
     (setq-default browse-url-firefox-new-window-is-tab t)
+    ;; (global-set-key (kbd "s-b") 'backward-word)
+    ;; (global-set-key (kbd "s-f") 'forward-word)
+    ;; (global-set-key (kbd "s-w") 'kill-ring-save)
+    ;; (global-set-key (kbd "s-d") 'kill-word)
+    ;; (global-set-key (kbd "s-t") 'transpose-words)
     (message "ew-os-particulars: Linux"))))
 ;; -------------------------------------------------------
 (provide 'ew-os-particulars)
