c69e18488dff9d3f6fde3c84ed7386faf63e74bf
Eric E. Westbrook <eew+git@lanl.gov>
2023-03-31 11:14:02 -0600

    ew-use-package.el: deleting packages seems bad

--- ew-use-package.el
+++ ew-use-package.el
@@ -18,7 +18,7 @@
 
 (use-package auto-package-update
   :config
-  (setq auto-package-update-delete-old-versions t)
+  ;; (setq auto-package-update-delete-old-versions t)
   (setq auto-package-update-hide-results t)
   (auto-package-update-at-time "03:00"))
 
