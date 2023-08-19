1c70198c3c752206c4bae6a229824e635df0e1ad
E. Westbrook <git@westbrook.io>
2023-08-18 21:00:26 -0600

    ew-fns-fix-date

--- ew-fns-fix-date.el
+++ ew-fns-fix-date.el
@@ -1,4 +1,4 @@
-;;; ew-fns-fixnextdate --- Summary
+;;; ew-fns-fix-date --- Summary
 ;;; Commentary:
 ;;; Code:
 ;; -------------------------------------------------------
@@ -11,49 +11,41 @@
    (format "%04d-%02d-%02d"
            (string-to-number (match-string 3))
            (string-to-number (match-string 1))
-           (string-to-number (match-string 2))
-           )
-   )
-  t
-  )
+           (string-to-number (match-string 2))))
+  t)
 
 (defun ew-fix-us-dates()
   "Fix all US-format dates in buffer to %F."
   (interactive)
-  (goto-char (point-min))
-  (while
-      (ew-fix-us-date)
-    )
-  )
+  (save-excursion
+    (goto-char (point-min))
+    (while (ew-fix-us-date))))
 
 (defun ew-make-org-date()
   "Make the next %F formatted date an org date."
   (interactive)
-  (re-search-forward "[^[]\\b\\([0-9]\\{4\\}-[0-9]\\{2\\}-[0-9]\\{2\\}\\)\\b")
-  (replace-match
-   (concat
-    "["
-    (match-string 1)
-    "]"
-    )
-   )
+  (re-search-forward "\\([^[]\\)\\b\\([0-9]\\{4\\}-[0-9]\\{2\\}-[0-9]\\{2\\}\\)\\b")
+  (replace-match (concat (match-string 1) "[" (match-string 2) "]" ))
   (backward-char 1)
   (org-ctrl-c-ctrl-c)
-  t
-  )
+  t)
 
 (defun ew-make-org-dates()
-  "Make all US-format dates into org dates."
+  "Make all %F formatted dates into org dates."
   (interactive)
-  (goto-char (point-min))
-  (while
-      (ew-make-org-date)
-    )
-  )
+  (save-excursion
+    (goto-char (point-min))
+    (while (ew-make-org-date))))
+
+(defun ew-fix-org-dates()
+  "Make US-format dates into %F dates and org-ify."
+  (interactive)
+  (ew-fix-us-dates)
+  (ew-make-org-dates))
 
 ;; -------------------------------------------------------
 ;; Local Variables:
 ;; mode: emacs-lisp
 ;; End:
-(provide 'ew-fns-fixnextdate)
-;;; ew-fns-fixnextdate.el ends here
+(provide 'ew-fns-fix-date)
+;;; ew-fns-fix-date.el ends here
