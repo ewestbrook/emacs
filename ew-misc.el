c31c6581e94b5a540636ab235cc063d1f34212e0
Eric Westbrook <westbrook@lanl.gov>
2023-12-15 23:10:55 -0700

    ew-misc.pl: screen xterm256; xterm-mouse-mode

--- ew-misc.el
+++ ew-misc.el
@@ -3,15 +3,14 @@
 ;;; Code:
 ;; -------------------------------------------------------
 
-(setq gc-cons-threshold 20000000)
-(load "url") (url-handler-mode t)
-(tool-bar-mode -1)
-(show-paren-mode t)
-(line-number-mode 1)
+(add-to-list 'term-file-aliases '("screen.xterm-256color" . "xterm-256color"))
 (column-number-mode 1)
-(global-font-lock-mode t)
 (global-auto-revert-mode 1)
+(global-font-lock-mode t)
+(line-number-mode 1)
+(load "url") (url-handler-mode t)
 (put 'downcase-region 'disabled nil)
+(setq gc-cons-threshold 20000000)
 (setq-default buffers-menu-max-size nil)
 (setq-default default-major-mode 'text-mode)
 (setq-default exec-path (append exec-path '("/usr/local/texlive/2018/bin")))
@@ -27,8 +26,11 @@
 (setq-default scroll-step 1)
 (setq-default transient-mark-mode t)
 (setq-default uniquify-buffer-name-style 'post-forward)
-(setq-default user-mail-address "")
 (setq-default user-full-name "")
+(setq-default user-mail-address "")
+(show-paren-mode t)
+(tool-bar-mode -1)
+(xterm-mouse-mode t)
 
 ;; -------------------------------------------------------
 ;; Local Variables:
