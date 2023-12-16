5020feab2fb91d0434c95327fb2912a3a2680b79
Eric Westbrook <westbrook@lanl.gov>
2023-12-15 23:09:30 -0700

    ew-lanl-proxy.el: if LCS, use proxyout.lcs.lanl.gov

--- ew-lanl-proxy.el
+++ ew-lanl-proxy.el
@@ -3,15 +3,23 @@
 ;;; Code:
 ;; -------------------------------------------------------
 
-(if (string-match "\\.lanl\\.gov$" (system-name))
+;; bug, see
+;; https://debbugs.gnu.org/cgi/bugreport.cgi?bug=11788
+
+(if (string-match "\\.lcs.lanl.gov$" (system-name))
     (setq url-proxy-services
           '(("no_proxy" . "^\\(localhost\\|10.*\\|192.168.29.*\\|172.55.*\\|lcs.lanl.gov\\|lanl.gov\\)")
-            ("http" . "proxyout.lanl.gov:8080")
-            ("https" . "proxyout.lanl.gov:8080"))))
+            ("http" . "proxyout.lcs.lanl.gov:8080")
+            ("https" . "proxyout.lcs.lanl.gov:8080")))
+  (if (string-match "\\.lanl\\.gov$" (system-name))
+      (setq url-proxy-services
+            '(("no_proxy" . "^\\(localhost\\|10.*\\|192.168.29.*\\|172.55.*\\|lcs.lanl.gov\\|lanl.gov\\)")
+              ("http" . "proxyout.lanl.gov:8080")
+              ("https" . "proxyout.lanl.gov:8080")))))
 
-;; -------------------------------------------------------
-;; Local Variables:
-;; mode: emacs-lisp
-;; End:
-(provide 'ew-lanl-proxy)
+  ;; -------------------------------------------------------
+  ;; Local Variables:
+  ;; mode: emacs-lisp
+  ;; End:
+  (provide 'ew-lanl-proxy)
 ;;; ew-lanl-proxy.el ends here
