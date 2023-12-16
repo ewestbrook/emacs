b3402f6f0c21833b817ddaa9bd8115da6c79c6e6
Eric Westbrook <westbrook@lanl.gov>
2023-12-16 00:24:38 -0700

    ew-elpy.el: whitespace

--- ew-elpy.el
+++ ew-elpy.el
@@ -4,21 +4,28 @@
 ;; -------------------------------------------------------
 
 (use-package elpy
+
   :ensure t
+
   :hook (elpy-mode . ew-elpy-init)
+
   :preface
+
   (defun ew-elpy-init()
     "Init elpy."
     (if (not (display-graphic-p))
         (progn
           (set-face-attribute 'highlight-indentation-face nil :background "grey15"))))
+
   (defun ew-elpy-goto-def-or-rgrep ()
     "Go to definition of thing at point or do an rgrep in project if that fails"
     (interactive)
     (condition-case nil (elpy-goto-definition-other-window)
       (error (elpy-rgrep-symbol (thing-at-point 'symbol)))))
+
   :bind
   (("C-c C-g" . ew-elpy-goto-def-or-rgrep))
+
   :init
   (elpy-enable))
 
