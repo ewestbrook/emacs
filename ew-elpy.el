779058df3eb5267bb20a8f09cb06b319b2007e84
Eric Westbrook <westbrook@lanl.gov>
2023-12-15 23:03:02 -0700

    ew-elpy.el

---
+++ ew-elpy.el
@@ -0,0 +1,31 @@
+;;; ew-elpy -- Summary
+;;; Commentary:
+;;; Code:
+;; -------------------------------------------------------
+
+(use-package elpy
+  :ensure t
+  :hook (elpy-mode . ew-elpy-init)
+  :preface
+  (defun ew-elpy-init()
+    "Init elpy."
+    (if (not (display-graphic-p))
+        (progn
+          (set-face-attribute 'highlight-indentation-face nil :background "grey15"))))
+  (defun ew-elpy-goto-def-or-rgrep ()
+    "Go to definition of thing at point or do an rgrep in project if that fails"
+    (interactive)
+    (condition-case nil (elpy-goto-definition-other-window)
+      (error (elpy-rgrep-symbol (thing-at-point 'symbol)))))
+  :bind
+  (("C-c C-g" . ew-elpy-goto-def-or-rgrep))
+  :init
+  (elpy-enable))
+
+
+;; -------------------------------------------------------
+;; Local Variables:
+;; mode: emacs-lisp
+;; End:
+(provide 'ew-elpy)
+;;; ew-elpy.el ends here
