c11e4fd767666e6821a77d0bb518629757f92ce6
Eric E. Westbrook <westbrook@lanl.gov>
2023-12-28 15:54:50 -0700

    ew-magit.el: misc improvements

--- ew-magit.el
+++ ew-magit.el
@@ -11,61 +11,49 @@
     (if (display-graphic-p)
 
         (progn
-
           (set-face-attribute 'magit-section-heading        nil :family "Noto Sans")
           (set-face-attribute 'magit-section-heading        nil :height 1.6)
-
           (set-face-attribute 'magit-diff-context           nil :background "#242424")
-
           (set-face-attribute 'magit-diff-added             nil :background "#204020")
           (set-face-attribute 'magit-diff-added             nil :foreground "#cccccc")
-
           (set-face-attribute 'magit-diff-removed           nil :background "#402020")
           (set-face-attribute 'magit-diff-removed           nil :foreground "#cccccc")
-
           (set-face-attribute 'magit-diff-context-highlight nil :background "#3a3a3a")
           (set-face-attribute 'magit-diff-removed-highlight nil :background "#633f3e")
           (set-face-attribute 'magit-diff-added-highlight   nil :background "#3a5c40"))
 
       (progn
-
         (set-face-attribute 'magit-log-date                    nil :foreground "grey50")
-
         (set-face-attribute 'magit-diff-hunk-heading           nil :background "grey40")
         (set-face-attribute 'magit-diff-hunk-heading-highlight nil :background "grey70")
-
         (set-face-attribute 'magit-section-highlight           nil :background "#333333")
-
         (set-face-attribute 'magit-diff-context-highlight      nil :background "grey15")
-
         (set-face-attribute 'magit-diff-added-highlight        nil :background "#224422")
         (set-face-attribute 'magit-diff-added-highlight        nil :foreground "#66ee66")
-
         (set-face-attribute 'magit-diff-added                  nil :background "#224422")
         (set-face-attribute 'magit-diff-added                  nil :foreground "#00aa00")
-
         (set-face-attribute 'diff-refine-added                 nil :background "#114411")
         (set-face-attribute 'diff-refine-added                 nil :foreground "#88ff88")
-
         (set-face-attribute 'magit-diff-removed-highlight      nil :background "#442222")
         (set-face-attribute 'magit-diff-removed-highlight      nil :foreground "#ee6666")
-
         (set-face-attribute 'magit-diff-removed                nil :background "#441111")
         (set-face-attribute 'magit-diff-removed                nil :foreground "#aa4444")
-
         (set-face-attribute 'diff-refine-removed               nil :background "#441111")
-        (set-face-attribute 'diff-refine-removed               nil :foreground "#ff8888")
-
-        )))
+        (set-face-attribute 'diff-refine-removed               nil :foreground "#ff8888"))))
 
   :hook (magit-status-mode . ew-magit-status-mode-init)
 
   :config
-  (setq magit-status-buffer-switch-function 'switch-to-buffer)
-  (setq magit-diff-refine-hunk 'all)
-  (setq magit-diff-paint-whitespace t)
-  (setq magit-diff-highlight-trailing t)
   (setq magit-diff-hide-trailing-cr-characters t)
+  (setq magit-diff-highlight-trailing t)
+  (setq magit-diff-paint-whitespace t)
+  (setq magit-diff-refine-hunk 'all)
+  (setq magit-status-buffer-switch-function 'switch-to-buffer)
+  (setq magit-section-initial-visibility-alist
+        '((stashes . hide)
+          (untracked . show)
+          (unstaged . show)
+          (unpushed . show)))
   (setq magit-repository-directories
         '(("~/git" . 5)
           ("/usr/local/eric/git" . 5))))
