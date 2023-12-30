2ef6dfac9900f087d29b015212e724458ec2a059
Eric E. Westbrook <westbrook@lanl.gov>
2023-12-29 20:36:22 -0700

    ew-lanl-proxy: properly exclude *.lanl.gov and *.lcs.lanl.gov

--- ew-lanl-proxy.el
+++ ew-lanl-proxy.el
@@ -8,12 +8,12 @@
 
 (if (string-match "\\.lcs.lanl.gov$" (system-name))
     (setq url-proxy-services
-          '(("no_proxy" . "^\\(localhost\\|10.*\\|192.168.29.*\\|172.55.*\\|lcs.lanl.gov\\|lanl.gov\\)")
+          '(("no_proxy" . "^\\(localhost\\|10\\.*\\|192\\.168\\.29\\..*\\|172\\.55\\..*\\|.*\\.lcs\\.lanl\\.gov\\|.*\\.lanl\\.gov\\)")
             ("http" . "proxyout.lcs.lanl.gov:8080")
             ("https" . "proxyout.lcs.lanl.gov:8080")))
   (if (string-match "\\.lanl\\.gov$" (system-name))
       (setq url-proxy-services
-            '(("no_proxy" . "^\\(localhost\\|10.*\\|192.168.29.*\\|172.55.*\\|lcs.lanl.gov\\|lanl.gov\\)")
+            '(("no_proxy" . "^\\(localhost\\|10\\.*\\|192\\.168\\.29\\..*\\|172\\.55\\..*\\|.*\\.lcs\\.lanl\\.gov\\|.*\\.anl\\.gov\\)")
               ("http" . "proxyout.lanl.gov:8080")
               ("https" . "proxyout.lanl.gov:8080")))))
 
