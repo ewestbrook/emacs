f77fb4ec272c4439753d9c566af9d3eaf76ca038
Eric E. Westbrook <westbrook@lanl.gov>
2024-01-30 11:08:02 -0700

    ew-misc.el: remove whitespace-cleanup

--- ew-misc.el
+++ ew-misc.el
@@ -3,7 +3,6 @@
 ;;; Code:
 ;; -------------------------------------------------------
 
-(add-hook 'before-save-hook 'whitespace-cleanup)
 (add-to-list 'term-file-aliases '("screen.xterm-256color" . "xterm-256color"))
 (column-number-mode 1)
 (global-auto-revert-mode 1)
