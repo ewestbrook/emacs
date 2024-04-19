53903c72c739b517a977a52229a7e6ed9bc7db3a
Eric E. Westbrook <westbrook@lanl.gov>
2024-04-19 13:03:52 -0600

    ew-citeproc.el

---
+++ ew-citeproc.el
@@ -0,0 +1,17 @@
+;;; ew-citeproc.el -- Summary
+;;; Commentary:
+;;; Code:
+;; -------------------------------------------------------
+
+(message "ew-citeproc.el")
+
+(use-package citeproc
+  :ensure t
+  :hook emacs-lisp-mode)
+
+;; -------------------------------------------------------
+;; Local Variables:
+;; mode: emacs-lisp
+;; End:
+(provide 'ew-citeproc)
+;;; ew-citeproc.el ends here
