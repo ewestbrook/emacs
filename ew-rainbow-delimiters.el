f6203190de4545c86f14dcf79e0a4ecaf87696ba
Eric E. Westbrook <westbrook@lanl.gov>
2024-04-19 10:02:51 -0600

    ew-rainbow-delimiters.el

---
+++ ew-rainbow-delimiters.el
@@ -0,0 +1,17 @@
+;;; ew-rainbow-delimiters.el -- Summary
+;;; Commentary:
+;;; Code:
+;; -------------------------------------------------------
+
+(message "ew-rainbow-delimiters.el")
+
+(use-package rainbow-delimiters-mode
+  :ensure t
+  :hook emacs-lisp-mode)
+
+;; -------------------------------------------------------
+;; Local Variables:
+;; mode: emacs-lisp
+;; End:
+(provide 'ew-rainbow-delimiters)
+;;; ew-rainbow-delimiters.el ends here
