6eabee7ef112c0591cc16328841ea5df061b6d96
Eric E. Westbrook <westbrook@lanl.gov>
2024-04-19 10:02:47 -0600

    ew-rainbow.el

---
+++ ew-rainbow.el
@@ -0,0 +1,17 @@
+;;; ew-rainbow.el -- Summary
+;;; Commentary:
+;;; Code:
+;; -------------------------------------------------------
+
+(message "ew-rainbow.el")
+
+(use-package rainbow-mode
+  :ensure t
+  :hook emacs-lisp-mode)
+
+;; -------------------------------------------------------
+;; Local Variables:
+;; mode: emacs-lisp
+;; End:
+(provide 'ew-rainbow)
+;;; ew-rainbow.el ends here
