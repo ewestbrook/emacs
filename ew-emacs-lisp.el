ed0d64fad7116b80fb9470b6fc5d18fa9aace098
Eric E. Westbrook <westbrook@lanl.gov>
2024-04-19 10:03:44 -0600

    ew-emacs-lisp.el

---
+++ ew-emacs-lisp.el
@@ -0,0 +1,18 @@
+;;; ew-emacs-lisp.el -- Summary
+;;; Commentary:
+;;; Code:
+;; -------------------------------------------------------
+
+(message "ew-emacs-lisp.el")
+
+;; (defun ew-emacs-lisp-init()
+;;   (message "ew-emacs-lisp-init()"))
+
+;; (add-hook 'emacs-lisp-mode-hook 'ew-emacs-lisp-init)
+
+;; -------------------------------------------------------
+;; Local Variables:
+;; mode: emacs-lisp
+;; End:
+(provide 'ew-emacs-lisp)
+;;; ew-emacs-lisp.el ends here
